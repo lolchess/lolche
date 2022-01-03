0aca805d9c3b3a6768deca83b76b603ea5c90c88
moon9ua <koom6@naver.com>
2022-01-03 17:07:41 +0900

    feat: cors-anywhere을 통해 통신 성공 및 간단한 api 구축

--- src/apis/endpoint.ts
+++ src/apis/endpoint.ts
@@ -1,1 +1,7 @@
-export const endpoint: string = "https://kr.api.riotgames.com/";
+// const proxy: string = "https://cors.bridged.cc/"; // NOTE: 회원가입 후 승인을 받아야하는 것 같아서 사용하지 않기로...
+const proxy: string = "https://cors-anywhere.herokuapp.com/"; // NOTE: https://github.com/Rob--W/cors-anywhere
+const url: string = "https://kr.api.riotgames.com/";
+
+export const endpoint: string = proxy + url;
+export const apiKey: string =
+  "?api_key=RGAPI-264e19a2-c4fa-4283-a805-36fcfca3335d"; // TODO: 추후 .env 등을 사용해서 감춰야 할 듯?
--- src/apis/tftSummonerName.ts
+++ src/apis/tftSummonerName.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
-import { endpoint } from "./endpoint";
+import { apiKey, endpoint } from "./endpoint";
 
-const summonerByNameEndpoint: string = "/tft/summoner/v1/summoners/by-name/";
+const summonerByNameEndpoint: string = "tft/summoner/v1/summoners/by-name/";
 
 export const getSummonerByName = async () => {
-  const res = await axios.get(endpoint + summonerByNameEndpoint + "므그아");
+  const res = await axios.get(
+    endpoint + summonerByNameEndpoint + "므그아" + apiKey
+  );
 
   console.log(res);
 };
