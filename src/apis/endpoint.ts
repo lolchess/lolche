fd887dc324e607e5650d42d64f7f56d94fb70b22
moon9ua <koom6@naver.com>
2022-01-04 16:05:01 +0900

    feat: 디자인을 제외한 간략한 랭크 페이지 구현

--- src/apis/endpoint.ts
+++ src/apis/endpoint.ts
@@ -1,5 +1,6 @@
 // const proxy: string = "https://cors.bridged.cc/"; // NOTE: 회원가입 후 승인을 받아야하는 것 같아서 사용하지 않기로...
-const proxy: string = "https://cors-anywhere.herokuapp.com/"; // NOTE: https://github.com/Rob--W/cors-anywhere
+// const proxy: string = "https://cors-anywhere.herokuapp.com/"; // NOTE: https://github.com/Rob--W/cors-anywhere
+const proxy: string = "https://api.allorigins.win/raw?url="; // NOTE: 429 에러로 인해... https://allorigins.win/ 로 옮김.
 const url: string = "https://kr.api.riotgames.com/";
 
 export const endpoint: string = proxy + url;
