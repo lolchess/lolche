0b7dbea67294c4c79e3775a809b0cb306356106d
moon9ua <koom6@naver.com>
2022-01-06 16:32:47 +0900

    feat: 사용자 닉네임이 없는 채팅 구현

--- src/components/Chatting/Chatting.tsx
+++ src/components/Chatting/Chatting.tsx
@@ -1,4 +1,12 @@
-import { addDoc, collection, onSnapshot } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  limitToLast,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import useInput from "../../hooks/useInput";
 import { dbService } from "../../myFirebase";
@@ -6,29 +14,33 @@ import { dbService } from "../../myFirebase";
 interface message {
   id: string;
   text?: string;
-}
+} // TODO: 에러를 피하기 위해서... 제대로(?) 하려면 다시 봐야할 듯
 
 export default function Chatting(): JSX.Element {
   const [input, setInput, onChangeInput] = useInput("");
-
   const [messages, setMessages] = useState<message[]>([]);
 
   useEffect(() => {
     if (!dbService) return;
 
-    const dbRef = collection(dbService, "messages");
-    onSnapshot(dbRef, (querySnapshot) => {
+    const q = query(
+      collection(dbService, "messages"),
+      orderBy("createdAt"),
+      limitToLast(10) // NOTE: 현재는 테스트를 위해 10개만 보이도록... 실제로는 채팅 줄 수에 따라 변경하면 될 듯
+    );
+
+    onSnapshot(q, (querySnapshot) => {
       const data = querySnapshot.docs.map((doc) => ({
         ...doc.data(),
         id: doc.id,
       }));
-
       setMessages(data);
     });
   }, []);
 
   const chatMsg = () => {
     addDoc(collection(dbService, "messages"), {
+      createdAt: serverTimestamp(),
       text: input,
     });
   };
