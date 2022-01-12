ab4df3aa4c885550cd595a339fa9b6f3eecf0502
moon9ua <koom6@naver.com>
2022-01-13 04:24:11 +0900

    fix: 네비게이션 서치바 디자인 수정

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,12 +1,52 @@
 import { useNavigate } from "react-router";
 import styled from "styled-components";
-import {
-  StyledLi,
-  StyledStack,
-  StyledInput,
-} from "../../styles/styles.component";
-
+import { StyledLi } from "../../styles/styles.component";
 import useInput from "../../hooks/useInput";
+
+function Navigation(): JSX.Element {
+  const navigate = useNavigate();
+  const [input, setInput, onChangeInput] = useInput("");
+
+  const onClickBtn = () => {
+    if (!input.length) return;
+
+    navigate("/UserPage/" + input);
+    setInput("");
+  };
+
+  const onKeyPress = async (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === "Enter") {
+      onClickBtn();
+    }
+  };
+
+  return (
+    <StyledHeader id="header-nav">
+      <StyledNav id="ul-nav">
+        <Logo onClick={() => navigate("/")}>loltoGG</Logo>
+        {/* <StyledLi id="li-home">home</StyledLi> */}
+        <StyledLi id="li-rank" onClick={() => navigate("/rank")}>
+          순위표
+        </StyledLi>
+        <StyledLi id="li-community" onClick={() => navigate("/community")}>
+          커뮤니티
+        </StyledLi>
+        <StyledLi id="li-chat" onClick={() => navigate("/Chatting")}>
+          채팅
+        </StyledLi>
+
+        <InputContainer>
+          <StyledInput2
+            value={input}
+            onChange={onChangeInput}
+            onKeyPress={onKeyPress}
+          />
+          <StyledIcon className="fas fa-search" onClick={onClickBtn} />
+        </InputContainer>
+      </StyledNav>
+    </StyledHeader>
+  );
+}
 
 const StyledHeader = styled.header`
   color: white;
@@ -32,46 +72,39 @@ const StyledNav = styled.ul`
   list-style: none;
 `;
 
-function Navigation(): JSX.Element {
-  const navigate = useNavigate();
-  const [input, setInput, onChangeInput] = useInput("");
-
-  const onClickBtn = () => {
-    if (!input.length) return;
-
-    navigate("/UserPage/" + input);
-    setInput("");
-  };
-
-  return (
-    <StyledHeader id="header-nav">
-      <StyledNav id="ul-nav">
-        <Logo onClick={() => navigate("/")}>loltoGG</Logo>
-        {/* <StyledLi id="li-home">home</StyledLi> */}
-        <StyledLi id="li-rank" onClick={() => navigate("/rank")}>
-          순위표
-        </StyledLi>
-        <StyledLi id="li-community" onClick={() => navigate("/community")}>
-          커뮤니티
-        </StyledLi>
-        <StyledLi id="li-chat" onClick={() => navigate("/Chatting")}>
-          채팅
-        </StyledLi>
-
-        <StyledStack width="300px">
-          <StyledInput value={input} onChange={onChangeInput} />
-          <StyledLi onClick={onClickBtn}>찾기</StyledLi>
-        </StyledStack>
-      </StyledNav>
-    </StyledHeader>
-  );
-}
-
 const Logo = styled.span`
   /* font-family: "SongMyung"; */
   font-family: "OrelegaOne";
   font-size: 27px;
   cursor: pointer;
+`;
+
+const InputContainer = styled.div`
+  width: 250px;
+  height: 33px;
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 15px;
+  border-radius: 10px;
+`;
+
+const StyledInput2 = styled.input`
+  height: 80%;
+  width: calc(95%);
+  border: none;
+  font-size: 15px;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const StyledIcon = styled.i`
+  font-size: 17px;
+  cursor: pointer;
+  color: #404040;
 `;
 
 export default Navigation;
