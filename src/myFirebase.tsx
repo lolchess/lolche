20cba6ea8688ac7f0c1a374ff27a465538d5885d
moon9ua <koom6@naver.com>
2022-01-17 16:40:11 +0900

    chore: firestore 사용량 초과로 인한 새로운 firebase init

--- src/myFirebase.tsx
+++ src/myFirebase.tsx
@@ -1,3 +1,4 @@
+/*
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
@@ -20,3 +21,30 @@ export const firebase = app;
 export const dbService = db;
 
 // export const realtimeDb = getDatabase(app);
+*/
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAnRU_oetqR4P_QWqvbdWhY6YKEyuGkWKE",
+  authDomain: "loltogg-77234.firebaseapp.com",
+  projectId: "loltogg-77234",
+  storageBucket: "loltogg-77234.appspot.com",
+  messagingSenderId: "1028693797298",
+  appId: "1:1028693797298:web:560c8617d5da4f54df5cab",
+  measurementId: "G-BT3X635HR6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const db = getFirestore();
+
+export const firebase = app;
+export const dbService = db;
